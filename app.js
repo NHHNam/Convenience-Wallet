17df3d833f6e61c9cdb804e7d7d2583b8beeb70d
hoainam2001 <nammaiyeutran@gmail.com>
2022-05-05 10:02:48 +0700

    update config sever
    toi mớii thêmm severr nên mọi ng pull về và thay đổi sao cho phù hợp với máy

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const db = require('./db')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+db.query("SELECT * FROM account", (err, results, fields) =>{
+  if(err) throw err
+  console.log(results[0].email)
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+const config = require('./config')
+
+// console.log(config.HOST)
+
+const connection = mysql.createConnection({
+//   host: "localhost",
+  host: config.HOST,
+  user: config.USER,
+  password: config.PASSWORD,
+  database: config.DATABASE,
+});
+
+connection.connect(err =>{
+  if(err) throw err
+});
+
+module.exports = connection
